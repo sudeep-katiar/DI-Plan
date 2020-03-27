48b118cb83188c412453b76e7897c4815ae7cf67
Sudeep Kumar Katiar <56839928+sudeep-katiar@users.noreply.github.com>
2020-03-27 10:16:34 +0530

    InstanceOf

---
+++ InstanceOf/src/com/di/instance/Dog1.java
@@ -0,0 +1,15 @@
+package com.di.instance;
+
+class Animal {
+}
+
+class Dog1 extends Animal {// Dog inherits Animal
+
+	public static void main(String args[]) {
+		
+		Dog1 d = new Dog1();
+		
+		System.out.println(d instanceof Animal);// true
+		
+	}
+}
---
+++ InstanceOf/src/com/di/instance/InstanceOf1.java
@@ -0,0 +1,20 @@
+package com.di.instance;
+
+/**
+ * The java instanceof operator is used to test whether the object is an instance of the specified type (class or subclass or interface).
+ * 
+ * The instanceof in java is also known as type comparison operator because it compares the instance with type.
+ * It returns either true or false.
+ * If we apply the instanceof operator with any variable that has null value, it returns false.
+ * **/
+public class InstanceOf1 {
+	
+	public static void main(String[] args) {
+		
+		InstanceOf1 inst = new InstanceOf1();
+		
+		System.out.println(inst instanceof InstanceOf1);// (obj instanceof class)
+		
+	}
+
+}
