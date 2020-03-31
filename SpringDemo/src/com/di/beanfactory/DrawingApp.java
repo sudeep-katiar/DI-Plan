bc7894a4e972817008740bde8a689fb38f7b53dc
Sudeep Kumar Katiar <56839928+sudeep-katiar@users.noreply.github.com>
2020-03-31 12:35:48 +0530

    Java Based Spring Bean Configuration

---
+++ SpringDemo/src/com/di/beanfactory/DrawingApp.java
@@ -0,0 +1,16 @@
+package com.di.beanfactory;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
+public class DrawingApp {
+	
+	public static void main(String[] args) {
+//		Triangle triangle = new Triangle();
+		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		Triangle triangle = (Triangle) factory.getBean("triangle");
+		triangle.draw();
+	}
+
+}
---
+++ SpringDemo/src/com/di/beanfactory/Triangle.java
@@ -0,0 +1,9 @@
+package com.di.beanfactory;
+
+public class Triangle {
+	
+	public void draw () {
+		System.out.println("triangle drawn");
+	}
+
+}
